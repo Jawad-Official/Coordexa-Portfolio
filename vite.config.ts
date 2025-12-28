62a140e29089effc978f3fb29413894eda4505e0
Jawad-Official <jawad.arman.official@gmail.com>
2025-12-28 17:43:58 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: true, // allows external access
     port: 8080,
+    allowedHosts: [
+      "coordexa.com",
+      "www.coordexa.com",
+      "coordexa-portfolio.onrender.com",
+    ],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
