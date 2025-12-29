181ab014b286ee50a85fff819985cedfe240d751
Jawad-Official <jawad.arman.official@gmail.com>
2025-12-29 16:54:49 +0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+declare global {
+  interface Window {
+    dataLayer: unknown[];
+    gtag: (...args: unknown[]) => void;
+  }
+}
+
+// This file is a module, so we need to export something to make it work
+export {};
